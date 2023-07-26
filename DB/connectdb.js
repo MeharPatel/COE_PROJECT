ac6d5681e44bffc0d2854af8b5ead2f4624ae78d
meharpatel2512@gmail.com <meharpatel2512@gmail.com>
2023-07-26 20:04:21 +0530

    Complete

--- DB/connectdb.js
+++ DB/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const url = "mongodb://0.0.0.0:27017/COE_Project"
-const liveUrl = 'mongodb+srv://meharpatel2512:C6mWpTwsox0SJike@cluster0.1myrfay.mongodb.net/admissionportal?retryWrites=true&w=majority'
+const liveUrl = 'mongodb+srv://meharpatel2512:C6mWpTwsox0SJike@cluster0.1myrfay.mongodb.net/Jobex?retryWrites=true&w=majority'
 
 
 const connectdb = () => {
